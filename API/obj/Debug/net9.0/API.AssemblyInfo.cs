a95f5059db4b6b09e56a5e675227a2d8c8f46464
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-06-27 16:50:04 +0800

    Feat: Add basket entity

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62dc79d7ccece74c9743a2b40e39334a33b3aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+721fd6c7360149c728c498bc1d3dc9a3b101de78")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
