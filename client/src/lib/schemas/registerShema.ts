146b472cef173b5d89a49563a24f50cd78e2355f
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-07-24 23:21:51 +0800

    Refactor: Removed the demonstration of server side password validation, back to client side

--- client/src/lib/schemas/registerShema.ts
+++ client/src/lib/schemas/registerShema.ts
@@ -1,16 +1,16 @@
 import { z } from "zod";
 
-// const passwordValidation = new RegExp(
-//   /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,}$/
-// );
+const passwordValidation = new RegExp(
+  /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^\w\d\s:])([^\s]){8,}$/
+);
 
 
 export const registerSchema = z.object({
   email: z.email(),
-  // password: z.string().regex(passwordValidation, {
-  //   message: 'Password must be at least 8 characters long and must contain 1 lowercase, 1 uppercase, 1 number and 1 special character.'
-  // }),
-  password: z.string() // let server handle the validation
+  password: z.string().regex(passwordValidation, {
+    message: 'Password must be at least 8 characters long and must contain 1 lowercase, 1 uppercase, 1 number and 1 special character.'
+  })
+
 })
 
 export type RegisterSchema = z.infer<typeof registerSchema>;
