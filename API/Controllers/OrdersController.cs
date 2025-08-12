705f13c426c48aedeec7af5df191522e3b5ef7cc
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-08-13 00:02:11 +0800

    Feat: Adding the Create Order logic in the controller

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -1,4 +1,6 @@
 using API.Data;
+using API.DTOs;
+using API.Entities;
 using API.Entities.OrderAggregate;
 using API.Extensions;
 using Microsoft.AspNetCore.Authorization;
@@ -31,5 +33,77 @@ public class OrdersController(StoreContext context) : BaseApiController
         if (order == null) return NotFound();
 
         return order;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Order>> CreateOrder(CreateOrderDto orderDto)
+    {
+        var basket = await context.Baskets.GetBasketWithItems(Request.Cookies["basketId"]);
+
+        if (basket == null || basket.Items.Count == 0) return BadRequest("Basket is empty or not found");
+
+        var items = CreateOrderItems(basket.Items);
+        if (items == null) return BadRequest("Some items are out of stock.");
+
+        var subtotal = items.Sum(x => x.Price * x.Quantity);
+
+        var deliveryFee = CalculateDeliveryFee(subtotal);
+
+        var order = new Order
+        {
+            OrderItems = items,
+            BuyerEmail = User.GetUsername(),
+            ShippingAddress = orderDto.ShippingAddress,
+            DeliveryFee = deliveryFee,
+            Subtotal = subtotal,
+            PaymentSummary = orderDto.PaymentSummary,
+            PaymentIntentId = basket.PaymentIntentId
+        };
+
+        context.Orders.Add(order);
+
+        context.Baskets.Remove(basket);
+        Response.Cookies.Delete("basketId");
+
+        var result = await context.SaveChangesAsync() > 0;
+
+        if (!result) return BadRequest("Problem creating order");
+
+        return CreatedAtAction(nameof(GetOrdersDetails), new { id = order.Id }, order);
+    }
+
+    private static long CalculateDeliveryFee(long subtotal)
+    {
+        return subtotal > 10000 ? 0 : 500;
+    }
+
+    private static List<OrderItem>? CreateOrderItems(List<BasketItem> items)
+    {
+        var orderItems = new List<OrderItem>();
+
+
+        foreach (var item in items)
+        {
+            if (item.Product.QuantityInStock < item.Quantity) return null;
+
+            var orderItem = new OrderItem
+            {
+                ItemOrdered = new ProductItemOrdered
+                {
+                    ProductId = item.ProductId,
+                    Name = item.Product.Name,
+                    PictureUrl = item.Product.PictureUrl,
+                },
+                Price = item.Product.Price,
+                Quantity = item.Quantity,
+            };
+
+            orderItems.Add(orderItem);
+
+            item.Product.QuantityInStock -= item.Quantity;
+        }
+
+        return orderItems;
+        
     }
 }
