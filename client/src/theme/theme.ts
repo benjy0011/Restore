466e85e19f5f81fecea187bbdbef7e98c690a4de
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-08-17 16:50:00 +0800

    Refactor: Font changed back to QuickSand

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -17,8 +17,8 @@ export const getTheme = (
 
     typography: {
       fontFamily: [
-        "Avenir",
         "Quicksand",
+        "Avenir",        
         "Roboto"
       ].join(", ")
     },
