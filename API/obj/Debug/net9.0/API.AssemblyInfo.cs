7f88737b3a5254644e3f8bc7ca504c7a0ec008af
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-08-19 23:29:19 +0800

    Refactor: Other files update

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f26dc1335331f1f69e9278eeeee101f7be01471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852a56a869a8bd2e626ff1bd3c2b3ce3d305d1dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
