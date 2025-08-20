d73f8b429d80a58a479665de37920c541ed536e0
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2025-08-20 23:30:49 +0800

    Refactor: Pre publish update

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+852a56a869a8bd2e626ff1bd3c2b3ce3d305d1dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e776f1fa4fd13c02900f4e70911c7648a3077796")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
